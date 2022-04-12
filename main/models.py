e2e92f8f10fce115d58ef187829ec79c12598257
SirMatix <janeta.mateusz@gmail.com>
2022-04-13 00:27:15 +0100

    change event model, added utils.py

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from datetime import datetime
 
 class Client(models.Model):
     name = models.CharField(max_length=128)
@@ -12,10 +13,12 @@ class Client(models.Model):
         return f"{self.name} {self.surname}"
 
 class Event(models.Model):
-    event = models.CharField('Event Name', max_length=128)
-    event_date = models.DateTimeField('Event Date')
-    address = models.CharField('Event Address', max_length=128)
+    title = models.CharField('Event Name', max_length=128)
     description = models.TextField(blank=True)
+    address = models.CharField('Event Address', max_length=128, blank=True)
+    start_date = models.DateTimeField('Start Date', default=datetime.now())
+    end_date = models.DateTimeField('End Date', default=datetime.now())
+    
 
     def __str__(self):
-        return self.name
+        return self.title
